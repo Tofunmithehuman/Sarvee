c3b35da6d7e65ba14bcaf19f99bd29f8cf24567e
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-11-16 16:47:40 +0100

    first commit

---
+++ src/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {
+        name: "Technology",
+        img: "https://plus.unsplash.com/premium_photo-1680985551009-05107cd2752c?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Laptop",
+        img: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Phone",
+        img: "https://images.unsplash.com/photo-1591337676887-a217a6970a8a?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+export default categories;
---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import categories from '../categories';
+import { Link } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import '../pages/styles/styles.css';
+
+function Home() {
+  return (
+    <div className='Home'>
+      <img src='https://res.cloudinary.com/dzzwvcapu/image/upload/c_scale,q_100,r_2,w_1024/v1700146528/Shop_Savvy_yxrnt5.jpg'/>
+      <div className='featured-products-container container mt-4'>
+          <h2>Last products</h2>
+          {/* last products here */}
+      
+        <div>
+          <Link to="/category/all" style={{textAlign: "right", display: "block", textDecoration: "none"}}>
+            See more {">>"}
+          </Link>
+        </div>
+      </div>
+      {/* sale banner */}
+      <div className='sale_banner--container mt-4'>
+        <img src='https://res.cloudinary.com/dzzwvcapu/image/upload/c_scale,q_100,r_2,w_1024/v1700146528/Shop_Savvy_yxrnt5.jpg' />
+      </div>
+      <div className='recent-products-container mt-4'>
+        <h2>Categories</h2>
+        <Row>
+          {categories.map((category) => (
+            <LinkContainer to={`/category/${category.name.toLocaleLowerCase()}`}>
+              <Col md={4}>
+                <div style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`, gap: "10px"}} className='category-tile'>
+                  {category.name}
+                </div>
+              </Col>
+            </LinkContainer>
+          ))}
+        </Row>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
