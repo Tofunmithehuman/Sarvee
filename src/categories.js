efe743f4385daef1d90dae3fad3a59cffdc6ac0c
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-12-20 22:10:45 +0100

    sucessfully integrated api

--- src/categories.js
+++ src/categories.js
@@ -8,7 +8,7 @@ const categories = [
         img: "https://images.unsplash.com/photo-1661956602944-249bcd04b63f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
-        name: "Fasihon",
+        name: "Fashion",
         img: "https://images.unsplash.com/photo-1628861997457-db259d8322ac?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ]
